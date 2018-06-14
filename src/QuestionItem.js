ed2c62f807b29014f056cc0a6b061e54b188bb1b
Leon Aves <leonaves1@gmail.com>
2018-06-14 17:45:33 +0100

    Show images in list.

--- src/QuestionItem.js
+++ src/QuestionItem.js
@@ -17,7 +17,7 @@ class QuestionItem extends React.Component {
   }
 
   render() {
-    const { question, correct_answer, incorrect_answers, tags } = this.props;
+    const { question, correct_answer, incorrect_answers, image, tags } = this.props;
     const { open } = this.state;
 
     return (
@@ -28,6 +28,10 @@ class QuestionItem extends React.Component {
             <li><strong>Correct answer:</strong> { correct_answer }</li>
             <li><strong>Incorrect answers:</strong> { incorrect_answers }</li>
             <li><strong>Tags:</strong> { tags }</li>
+            <li>
+              <strong>Images:</strong>
+              { image.split(',').map(src => <img src={ `/uploads/${ src }` } />) }
+            </li>
           </ul>
         )}
       </li>
